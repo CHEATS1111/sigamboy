0a44169b17d92052d27bec8d058f699483962481
CHEATS1111 <mail@mwq-server.ru>
2025-10-26 13:59:10 +0500

    Fix duplicate getCookie function

--- app/admin/chats/page.tsx
+++ app/admin/chats/page.tsx
@@ -144,17 +144,6 @@ export default function AdminChatsPage() {
     document.cookie = name + '=' + value + ';' + expires + ';path=/'
   }
 
-  const getCookie = (name: string) => {
-    const nameEQ = name + '='
-    const ca = document.cookie.split(';')
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i]
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
-    }
-    return null
-  }
-
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault()
     if (password === '33336020') {
